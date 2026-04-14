cb697689854a3c773524ed099f4dc34aa6c33f7e
Prajakta Ukirde <165979795+prajaktaukirde@users.noreply.github.com>
2026-04-14 16:45:36 +0530

    Time: 43 ms (24.75%) | Memory: 14 MB (81.05%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    vector<int> twoSum( vector<int>& nums, int target) {
-    map<int,int>mpp;
-    for  (int i=0;i<nums.size();i++){
-        int a =nums[i];
-        int more=target-a;
-        if (mpp.find (more)!=mpp.end()){
-            return {mpp[more],i};
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i =0; i<nums.size();i++ ){
+            for (int j=i+1 ; j<nums.size();j++  ){
+                if (nums[i]+nums[j] == target){
+                    return {i,j};
+                }
+            }
         }
-        mpp[a] = i;
-    }
-    return {};  
+        return{};
+        
     }
 };
