54fd8cf75fa40affa48f8486bbac89292a3f46dc
Prajakta Ukirde <165979795+prajaktaukirde@users.noreply.github.com>
2026-06-14 13:22:17 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (51.62%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 0) return 0;
+        if (nums.empty()) return 0;
 
-        int officer = 0;  
-        int cm = 1;        
-        int uni = 1;      
+        int j = 0;
+        int i = 1;
+        int uni = 1;
 
-        while(cm < n) {
-            if(nums[cm] == nums[cm - 1]) {
-                cm++;   
-            }
-            else {
-                officer++;
-                nums[officer] = nums[cm]; 
-                cm++;
+        while (i < nums.size()) {
+
+            if (nums[i] != nums[j]) {
+                j++;
+                nums[j] = nums[i];
                 uni++;
             }
+
+            i++;
         }
+
         return uni;
     }
 };
