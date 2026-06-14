49cf7d691cadd803d0c7ccec0fb7ff47dbf03da3
Prajakta Ukirde <165979795+prajaktaukirde@users.noreply.github.com>
2026-06-14 14:15:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (71.84%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-        vector<int> res(m + n); 
-        int i = 0, j = 0, k = 0;
+    void merge(vector<int>& nums1, int m,
+               vector<int>& nums2, int n) {
 
-      
-        while (i < m && j < n) {
-            if (nums1[i] < nums2[j]) {
-                res[k] = nums1[i];
-                i++;
-            } else {
-                res[k] = nums2[j];
-                j++;
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
             }
-            k++;
-        }
-        while (i < m) {
-            res[k] = nums1[i];
-            i++;
-            k++;
-        }
-        while (j < n) {
-            res[k] = nums2[j];
-            j++;
-            k++;
+            else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+
+            k--;
         }
 
-        for (int x = 0; x < m + n; x++) {
-            nums1[x] = res[x];
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
     }
 };
