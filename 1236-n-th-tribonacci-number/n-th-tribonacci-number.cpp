d6649b17c4e0c7eb34ab6e79b6a78c8df13fb648
Prajakta Ukirde <165979795+prajaktaukirde@users.noreply.github.com>
2026-07-19 22:01:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (25.61%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    vector<int> dp;
+    int tribonacci(int n) {
 
-    int solve(int n) {
-   
+        vector<int> dp(n + 1);
+
         if (n == 0)
             return 0;
 
         if (n == 1 || n == 2)
             return 1;
 
-        if (dp[n] != -1)
-            return dp[n];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
 
-        dp[n] = solve(n - 1) + solve(n - 2) + solve(n - 3);
-
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
         return dp[n];
-    }
-
-    int tribonacci(int n) {
-        dp.resize(n + 1, -1);
-        return solve(n);
     }
 };
