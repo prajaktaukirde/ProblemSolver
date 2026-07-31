cd15328348a699253fced7f1d8c9da6751d7f6cb
Prajakta Ukirde <165979795+prajaktaukirde@users.noreply.github.com>
2026-07-31 14:04:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (37.12%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+
+        int ans = x ^ y;
+        int count = 0;
+        while(ans){
+            ans = ans & (ans-1);
+            count++;
+        }
+        return count;
+    }
+};
