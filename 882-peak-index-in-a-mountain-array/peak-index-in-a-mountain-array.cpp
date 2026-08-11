945296aaecc830c6c74c4a2d57069c91365dd254
Prajakta Ukirde <165979795+prajaktaukirde@users.noreply.github.com>
2026-08-11 21:45:01 +0530

    Time: 0 ms (100.00%) | Memory: 63.6 MB (49.12%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int low = 0;
-        int high = arr.size() - 1;
-        int res=-1;
-        while(low<=high){
-                int mid=(low+high)/2;
-                if (arr[mid]<arr[mid+1]){
-                    low=mid+1;
-                }
-                else{
-                    res=mid;
-                    high = mid-1;
-                }
-            }
-            return res;
+        int l = 0, r = arr.size() - 1;
+
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+
+            if(arr[mid] < arr[mid + 1])
+                l = mid + 1;
+            else
+                r = mid;
         }
-    
+
+        return l;
+    }
 };
